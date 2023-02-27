937f47b947feac13f998a0dc96ff84180438f0cc
leonard-ramos27 <117474533+leonard-ramos27@users.noreply.github.com>
2023-02-28 03:07:59 +0800

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import Header from './components/header'
+import User from './components/user'
+
+import './scss/app.scss'
+
+const app = async () => {
+    document.getElementById('header').innerHTML = Header();
+    document.getElementById('user').innerHTML = await User();
+}
+
+app();
